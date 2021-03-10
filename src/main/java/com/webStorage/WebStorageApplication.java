c238ada812493276dc9cfaf26abf9ebd1452d6b9
kil6176 <kil6176@naver.com>
2021-03-10 16:49:17 +0900

    vue + spring boot 빈 프로젝트

---
+++ src/main/java/com/webStorage/WebStorageApplication.java
@@ -0,0 +1,13 @@
+package com.webStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webStorage/WebStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
